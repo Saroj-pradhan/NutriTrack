8403870040f7365430b5132447002d5bc50d73ae
saroj <sarojpradhan7655@gmail.com>
2026-03-28 16:08:09 +0530

    fix:cors problem

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,7 @@ const NutritionRoutes = require("./routes/NutritionRoutes")
 const app = express();
 // app.use(express.json());
 
-app.use(cors({
-    origin:["http://localhost:5173","https://nutritrack-pink.vercel.app"],
-  credentials: true
-}))
+app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
