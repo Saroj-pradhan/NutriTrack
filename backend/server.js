5ac28fd5f6f6a1451d93ac08742de2bc81c7491d
saroj <sarojpradhan7655@gmail.com>
2026-03-28 13:47:19 +0530

    fix:cors

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const app = express();
 // app.use(express.json());
 
 app.use(cors({
-    origin:["http://localhost:5173","https://nutritrack-pink.vercel.app/"],
+    origin:["http://localhost:5173","https://nutritrack-pink.vercel.app"],
   credentials: true
 }))
 app.use(express.json({ limit: "10mb" }));
