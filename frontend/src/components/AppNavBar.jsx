84ab4a65f202afb8e6448a62c12b6e8c00c06405
saroj <sarojpradhan7655@gmail.com>
2026-04-29 20:31:51 +0530

    feat:created circularProgress bar

---
+++ frontend/src/components/AppNavBar.jsx
@@ -0,0 +1,40 @@
+import { GoHome } from "react-icons/go";
+import { TbBrandGoogleAnalytics } from "react-icons/tb";
+import { IoSettingsOutline } from "react-icons/io5";
+import { MdOutlineQrCodeScanner } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
+
+function AppNavBar() {
+    const navigate = useNavigate();
+    function NavigateToupload(){
+    navigate("/upload")
+    }
+    function NavigateToHome(){
+    navigate("/home")
+    }
+    function NavigateToAnalyse(){
+        navigate("/analyse")
+    }
+  return (
+     <div className="fixed  bottom-0 left-0 w-[100%] flex items-center justify-evenly text-white backdrop-blur-sm md:ml-5 md:mr-7">
+     
+      <div className="flex flex-col items-center" onClick={NavigateToHome}>
+        <GoHome size={25}/>
+        <p>Home</p>
+      </div>
+      <div className="flex flex-col items-center" onClick={NavigateToupload} >    
+      <MdOutlineQrCodeScanner   size={25} />
+      <p>Log</p>
+      </div>
+      <div className="flex flex-col items-center" onClick={NavigateToAnalyse}>
+        <TbBrandGoogleAnalytics size={25} />
+        <p>Analytics</p>
+      </div>
+       <div className="h-14 w-14 bg-white rounded-full sm:hidden flex justify-center items-center ">
+<p className="text-6xl text-black">+</p>
+      </div>
+    </div>
+  )
+}
+
+export default AppNavBar
